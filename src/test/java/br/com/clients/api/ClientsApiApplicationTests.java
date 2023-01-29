7df09fa166bfface777ad085dc889cab24145a04
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-01-29 13:32:13 -0300

    first commit

---
+++ src/test/java/br/com/clients/api/ClientsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clients.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
